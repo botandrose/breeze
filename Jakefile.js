65008b0220b4dbed4f9449cb859aa6886bb7267e
Kathryn Stoddard <kathryn.stoddard@gmail.com>
2014-04-05 13:00:00 -0700

    Adding updates to Jakefile to call 15 minute data and adding fifteenMinute.js module for Jake calls

--- Jakefile.js
+++ Jakefile.js
@@ -1,5 +1,9 @@
-var getData = require('./server5');
+var getData = require('./fifteenMinute');
 
-desc('This is the default task.');
-task('default', [], function () {
+desc('This is to fetch Data every 15 minutes from forecast.io.');
+task('getData15', [], function () {
+});
+
+desc('This is to fetch Data every 24 hours from forecast.io.');
+task('getData24Hr', [], function () {
 });
